4b74d195192a57699017d0d0fee0bf3e8a7beabc
ranjitha-rudrappa <150125133+ranjitha-rudrappa@users.noreply.github.com>
2023-11-07 15:39:39 +0530

    Update TestJavaCollections.java

--- TestJavaCollections.java
+++ TestJavaCollections.java
@@ -6,8 +6,9 @@ public static void main(String [] args)
 ArrayList<String> list=new ArrayList<String>();//Creating arraylist  
 list.add("Ravi");//Adding object in arraylist  
 list.add("Vijay");  
-list.add("Ravi");  
-list.add("Ajay");  
+list.add("Ramesh");  
+list.add("Ajay"); 
+  list.add("Teja");
 //Traversing list through Iterator  
 Iterator itr=list.iterator();  
 while(itr.hasNext()){  
